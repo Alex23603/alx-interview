289691863da76a39178c32fb23b10dc82f3e2b24
Alex23603 <alexx24603@gmail.com>
2024-10-06 11:03:12 -0700

    0-main.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,21 +1,25 @@
+#!/usr/bin/python3
+"""
+Module 0-pascal_triangle
+Generates Pascal's triangle of a given size n.
+"""
+
 def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing Pascal's triangle.
+    :param n: size of Pascal's triangle (int)
+    :return: List of lists of integers (Pascal's triangle)
+    """
     if n <= 0:
         return []
-    
-    # Initialize the first row of Pascal's triangle
+
     triangle = [[1]]
-    
-    # Build the triangle row by row
+
     for i in range(1, n):
-        # Start each row with a 1
         row = [1]
-        
-        # Calculate the in-between values as the sum of the two values above
         for j in range(1, i):
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        
-        # End each row with a 1
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
         row.append(1)
         triangle.append(row)
-    
+
     return triangle
