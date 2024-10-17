92f64e856b40709637b364e5b69e234392a6ea99
Alex23603 <alexx24603@gmail.com>
2024-10-17 11:46:48 -0700

    0-minoperations.py2

--- 0x02-minimum_operations/0-main.py
+++ 0x02-minimum_operations/0-main.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
 """
-Main file for testing
+Main file for testing the minOperations function
 """
 
 minOperations = __import__('0-minoperations').minOperations
 
 n = 4
-print("Min number of operations to reach {} char: {}".format(n, minOperations(n)))
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
 
 n = 12
-print("Min number of operations to reach {} char: {}".format(n, minOperations(n)))
-
-n = 9
-print("Min number of operations to reach {} char: {}".format(n, minOperations(n)))
-
-n = 15
-print("Min number of operations to reach {} char: {}".format(n, minOperations(n)))
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python3
+"""
+This module defines the minOperations function
+"""
+
 def minOperations(n):
-    """Calculates the fewest number of operations to reach n characters using 'Copy All' and 'Paste'."""
+    """
+    Calculates the fewest number of operations needed to result in exactly n 'H' characters.
+
+    Args:
+        n (int): The target number of 'H' characters.
+
+    Returns:
+        int: The minimum number of operations needed to achieve n characters, or 0 if impossible.
+    """
     if n < 2:
         return 0
 
     operations = 0
-    factor = 2
+    divisor = 2
 
     while n > 1:
-        while n % factor == 0:
-            operations += factor
-            n //= factor
-        factor += 1
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
 
     return operations
